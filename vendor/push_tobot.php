21f12d7b7411ec1594e40a54859369308ca14b03
Nudpakun Leechaikul <nudpakun.l@om4you.com>
2018-06-20 01:58:39 +0700

    - Add new to_bot.php to contact with Line Notify

--- vendor/push_tobot.php
+++ vendor/push_tobot.php
@@ -53,7 +53,7 @@ $httpClient = new CurlHTTPClient(LINE_MESSAGE_ACCESS_TOKEN);
 $bot = new LINEBot($httpClient, array('channelSecret' => LINE_MESSAGE_CHANNEL_SECRET));
 
 // userId
-$userId = ' userId ของผู้ใช้ที่เราต้องการส่งข้อความ push ไปแสดง ';
+$userId = 'U8e65516071e4b5d29c578c4f285ac66c';
 // ทดสอบส่ง push ข้อความอย่างง่าย
 $textPushMessage = 'สวัสดีครับ';
 $messageData = new TextMessageBuilder($textPushMessage);
