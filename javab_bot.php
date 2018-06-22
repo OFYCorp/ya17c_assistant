8a0248c8fa6ed4722c55411470c579d688df381a
Nudpakun Lee <nudpakun.l@om4you.com>
2018-06-22 15:38:32 +0700

    Java Bot

--- javab_bot.php
+++ javab_bot.php
@@ -57,8 +57,8 @@ if (sizeof($request_array['events']) > 0) {
             //$reply_message = iconv("tis-620","utf-8",$reply_message);
             $data = [
                 'replyToken' => $reply_token,
-                'messages' => [['type' => 'text', 'text' => $reply_message]]
-//                'messages' => [['type' => 'image', 'originalContentUrl' => 'http://www.yarisativ2017.club/img/yarisativ-logo.jpg', 'previewImageUrl' => 'http://www.yarisativ2017.club/img/yarisativ-logo.jpg', 'animated' => false]]
+//                'messages' => [['type' => 'text', 'text' => $reply_message]]
+                'messages' => [['type' => 'image', 'originalContentUrl' => 'https://i2.wp.com/beebom.com/wp-content/uploads/2016/01/Reverse-Image-Search-Engines-Apps-And-Its-Uses-2016.jpg?resize=640%2C426', 'previewImageUrl' => 'https://i2.wp.com/beebom.com/wp-content/uploads/2016/01/Reverse-Image-Search-Engines-Apps-And-Its-Uses-2016.jpg?resize=640%2C426', 'animated' => false]]
 //                'messages' =>
 //                    [
 //                        [
