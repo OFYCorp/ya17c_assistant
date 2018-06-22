9ed7ddca9ed1103300ad5fff040db2ea6bb31652
Jiraphat Siriwasinchai <jiraphat.s@om4you.com>
2018-06-23 03:32:59 +0700

    -test

--- javab_bot.php
+++ javab_bot.php
@@ -26,9 +26,9 @@ if (sizeof($request_array['events']) > 0) {
                 } else if ($text == 'Yaris') {
                     $reply_message = 'มีทั้งหมด 4 รุ่น ดังนี้                           - 2 J Eco CVT ราคา 489,000 บาท - 2 J CVT        ราคา 539,000 บาท   - 2 E CVT        ราคา 569,000 บาท   - 2 G CVT       ราคา 619,000 บาท';
                 }
-//                else if ($text == 'Yaris ATIV') {
-//                    $reply_message = 'มีทั้งหมด 5 รุ่น ดังนี้                           - 1.2 J Eco CVT ราคา 479,000 บาท - 1.2 J CVT ราคา 529,000 บาท - 1.2 E CVT ราคา 559,000 บาท - 1.2 G CVT ราคา 609,000 บาท- 1.2 S CVT ราคา 635,000 บาท';
-//                }
+                else if ($text == 'Yaris ATIV') {
+                    $reply_message = 'มีทั้งหมด 5 รุ่น ดังนี้                           - 1.2 J Eco CVT ราคา 479,000 บาท - 1.2 J CVT ราคา 529,000 บาท - 1.2 E CVT ราคา 559,000 บาท - 1.2 G CVT ราคา 609,000 บาท- 1.2 S CVT ราคา 635,000 บาท';
+                }
                 if ($text == 'Yaris ATIV มีกี่สี') {
                     $reply_message = 'มีทั้งหมด 7 สี ดังนี้
                     - Dark Blue Mica Metallic
