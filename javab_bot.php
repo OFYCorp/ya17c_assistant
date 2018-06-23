99f089a5b0078bd2aa57a504bcd310ab26d57120
Nudpakun Lee <nudpakun.l@om4you.com>
2018-06-23 14:20:40 +0700

    Java Bot

--- javab_bot.php
+++ javab_bot.php
@@ -150,8 +150,7 @@ if (sizeof($request_array['events']) > 0) {
                             ]
                     ]]
                 ];
-            } else if ($text == 'Yaris มีสีอะไรบ้าง?') {
-                $reply_message = "";
+            } else if ($text == 'Yaris มีกี่สี?') {
                 $data = [
                     'replyToken' => $reply_token,
                     // Text
